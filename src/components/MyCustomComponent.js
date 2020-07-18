f1e74cd8021b42201f3e6749b3b0c3ced00f86da
flowerpowder6725 <flowerpowder6725@gmail.com>
2020-07-18 09:38:23 +0000

    some more debug code

---
+++ src/components/MyCustomComponent.js
@@ -0,0 +1,17 @@
+
+export default 
+ {
+    render: function (createElement) {
+      return createElement(
+        'h' + this.level,   // tag name
+        this.$slots.default // array of children
+      )
+    },
+    props: {
+      level: {
+        type: Number,
+        required: true
+      }
+    }
+  }
+  
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,55 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+     <div>
+  <VueToggles
+  @click="value = !value"
+  :value="value"
+  height="30"
+  width="90"
+  checkedText="On"
+  uncheckedText="Off"
+  checkedBg="#b4d455"
+  uncheckedBg="lightgrey"
+       />
   </div>
+
+   
+  <VueToggles
+  @click="value = !value"
+  :value="value"
+  height="30"
+  width="90"
+  checkedText="On"
+  uncheckedText="Off"
+  checkedBg="#b4d455"
+  uncheckedBg="lightgrey"
+       />
+
+       <anchored-heading :level="1">Hello this is my world!</anchored-heading>
+  
+  </div>
+
+ 
+
+  
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 
+import VueToggles from 'vue-toggles';
+
+import AnchorHeader from "@/components/MyCustomComponent"
+
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    VueToggles,
+    "anchored-heading": AnchorHeader
+
   }
 };
 </script>
